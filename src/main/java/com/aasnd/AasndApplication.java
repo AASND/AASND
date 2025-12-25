e726742071098f705ef7498dbd4bfd57ff3aaa28
YoonJiwon <gaden119000@gmail.com>
2025-12-25 20:24:29 +0900

    init project

---
+++ src/main/java/com/aasnd/AasndApplication.java
@@ -0,0 +1,13 @@
+package com.aasnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AasndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AasndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aasnd/AasndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aasnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AasndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
